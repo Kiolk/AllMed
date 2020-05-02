5d9cb648e0508f109325304bb834f420c563316e
Yauheni Slizh <tyteishi@gmail.com>
2020-05-03 00:11:23 +0300

    Add dependencies

--- app/src/main/java/com/github/kiolk/allmed/MainActivity.kt
+++ app/src/main/java/com/github/kiolk/allmed/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.kiolk.allmed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
